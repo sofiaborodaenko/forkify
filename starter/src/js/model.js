6b853a96a0d7b374191eda14e798d48b07bc6d65
Sofia Borodaenko <soph.bor06@gmail.com>
2025-06-14 12:55:32 -0400

    added config file and helper file

--- starter/src/js/model.js
+++ starter/src/js/model.js
@@ -1,18 +1,15 @@
+import { API_URL } from "./config.js";
+import { getJSON } from "./helpers.js";
+
 export const state = {
   recipe: {},
 };
 
+// Gets the recipe from the api and adds it to the state
 export const loadRecipe = async function (id) {
   try {
-    const res = await fetch(
-      `https://forkify-api.jonas.io/api/v2/recipes/${id}`
-    );
+    const data = await getJSON(`${API_URL}/${id}`);
 
-    const data = await res.json();
-
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-
-    console.log(res, data);
     // rewriting the variables
     const { recipe } = data.data;
     state.recipe = {
@@ -28,6 +25,6 @@ export const loadRecipe = async function (id) {
 
     console.log(state.recipe);
   } catch (err) {
-    alert(err);
+    console.error(`${err} 💣💣💣💣`);
   }
 };
