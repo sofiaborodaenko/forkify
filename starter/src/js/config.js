21887624c3f82df076cdfe627b54ef4e32818246
Sofia Borodaenko <soph.bor06@gmail.com>
2025-06-16 14:26:09 -0400

    recipe addition function

--- starter/src/js/config.js
+++ starter/src/js/config.js
@@ -3,3 +3,5 @@
 export const API_URL = "https://forkify-api.jonas.io/api/v2/recipes";
 export const TIMEOUT_SEC = 20;
 export const RESULTS_PER_PAGE = 10;
+export const KEY = "119daed1-6dd4-4c6e-8a15-27d463821273";
+export const MODAL_CLOSE_SEC = 2.5;
--- starter/src/js/helpers.js
+++ starter/src/js/helpers.js
@@ -21,3 +21,24 @@ export const getJSON = async function (url) {
     throw err;
   }
 };
+
+// Sends the given new recipe to the api
+export const sendJSON = async function (url, uploadData) {
+  try {
+    const fetchPro = fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(uploadData),
+    });
+
+    const res = await Promise.race([fetchPro, timeout(TIMEOUT_SEC)]);
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    return data;
+  } catch (err) {
+    throw err;
+  }
+};
