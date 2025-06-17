71ea8da410ced13c808d5716adcd4e157015462d
Sofia Borodaenko <soph.bor06@gmail.com>
2025-06-17 18:31:06 -0400

    fixed the package.json

--- docs/src/js/views/paginationView.js
+++ docs/src/js/views/paginationView.js
@@ -32,6 +32,7 @@ class PaginationView extends View {
         <button data-goto="${
           curPage + 1
         }" class="btn--inline pagination__btn--next">
+        
             <span>Page ${curPage + 1}</span>
             <svg class="search__icon">
               <use href="${icons}#icon-arrow-right"></use>
